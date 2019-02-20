b06a37c78bd8fa6709083f7242d14e303b7f0b83
Max Forsman <max.forsman@tieto.com>
2019-02-20 15:01:42 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This program will read a list of imgur album hashes and download a random image from a random album.
+Config file should be stored under ~/.config/artthief/albums.conf
+The list should contain imgur album hashes separated by newline.
+I.e. if the imgur link is `https://imgur.com/a/A123BC456` the entry should contain only `A123BC456`
---
+++ artthief.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+from os.path import expanduser
+import os
+import random
+import json
+import requests
+import urllib.request
+import datetime
+from pathlib import Path
+import argparse
+
+API_URL = "https://api.imgur.com/3"
+ALBUMS = []
+
+#TODO: fix paths
+def init():
+    global parser
+    parser = argparse.ArgumentParser(description='Download a random image from a list of imgur album hashes')
+    parser.add_argument('--clean', '-c', action='store_true',
+                        help='Cleans up old library images before running')
+
+    global BASE_DIR
+    global DOWNLOAD_DIR
+    BASE_DIR = Path(expanduser("~")) / '.config/artthief'
+    DOWNLOAD_DIR = BASE_DIR / 'library'
+    #TODO: Create library dir
+    if not DOWNLOAD_DIR.is_dir():
+        os.mkdir(DOWNLOAD_DIR)
+    config = BASE_DIR / 'albums.conf'
+    with open(config, "r") as f:
+        for link in f:
+            ALBUMS.append(link.rstrip())
+    return
+
+def set_base_dir():
+    return    
+
+def get_random_album_hash(albums):
+    return albums[random.randint( 0, len(ALBUMS)-1 ) ]
+
+def build_album_url_from_hash( album_hash ):
+    output = API_URL + '/album/' + album_hash
+    return output
+
+def get_json_data_from_response(response):
+    return json.loads(response.content)['data']
+
+def get_random_image_from_album_json(json_data):
+    count = json_data['images_count']
+    r_index = random.randint(0, count-1)
+    image = json_data['images'][r_index]
+    return image
+
+def download_random_image_from_albums(albums):
+    album_hash = get_random_album_hash(albums)
+    url = build_album_url_from_hash(album_hash)
+    r = requests.get(url, headers={'Authorization': 'Client-ID 8632b98a864f29d'})
+    data = get_json_data_from_response(r)
+    image = get_random_image_from_album_json(data)
+    title = create_file_name(image, album_hash)
+    download_image(image, title)
+
+def download_image(image_json, title):
+    path = DOWNLOAD_DIR / title
+    urllib.request.urlretrieve (image_json['link'], path)
+    print(path)   
+
+def create_file_name(image_json, album_hash):
+    path = str(datetime.date.today()) + album_hash + '-' + image_json['id']
+    title = image_json['title'] 
+    if title:
+        title = ''.join(e for e in title if e.isalnum())
+        path += '-' +title
+    path += '.jpg'
+    
+    #path = path.replace(' ','')
+    return path
+
+def clean():
+    for filename in os.listdir(DOWNLOAD_DIR):
+            if filename.endswith(".jpg"):
+                os.remove(DOWNLOAD_DIR / filename)
+
+def main():
+    init()
+    args = parser.parse_args()
+    if args.clean:
+        clean()
+    download_random_image_from_albums(ALBUMS)
+    
+main()
